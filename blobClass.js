18f775e1802b476e8f494b2c55b36d981ffcf6b7
Jo Lee <jolee224@gmail.com>
2023-03-12 19:24:40 -0400

    nyancat

--- blobClass.js
+++ blobClass.js
@@ -1,36 +1,35 @@
-
 class Blobby {
-  constructor(x, y) {
+  constructor(x, y, img) {
     this.x = x;
     this.y = y;
     this.noiseMax = 1;
     this.zoff = 0;
     this.radiusMin = 20;
     this.radiusMax = 50;
+    this.textureImg = "asteroid.jpg";
   }
+
   update() {
     this.x = random(width);
     this.y = random(height);
   }
+
   draw() {
-    // background(255, 0, 0, 255);
-    // noFill();
-    push(); // save the current transformation matrix
+    push();
     translate(0, -100);
     beginShape();
+    
     for (let angle = 0; angle < TWO_PI; angle += 0.1) {
       let xoff = map(cos(angle), -1, 1, 0, this.noiseMax);
       let yoff = map(sin(angle), -1, 1, 0, this.noiseMax);
       let radius = map(noise(xoff, yoff, this.zoff), 0, 1, this.radiusMin, this.radiusMax);
       let x = radius * cos(angle);
       let y = radius * sin(angle);
-      let distance = dist(x, y, 0, 0);
-      let gray = map(distance, 0, this.radiusMax, 255, 0);
-      stroke(gray);
+      
       curveVertex(x, y);
     }
     endShape(CLOSE);
-    pop(); // restore the previous transformation matrix
+    pop();
     this.zoff += 0.01;
   }
 }
--- sketch.js
+++ sketch.js
@@ -8,10 +8,8 @@ let amp;
 let fft;
 let freqBins = 64;
 let angle = 0;
-
-function preload() {
-  song = loadSound("./fluffy.mp3");
-}
+let img;
+let catimg;
 
 function togglePlayer() {
   if (!song.isPlaying()) {
@@ -33,7 +31,7 @@ function setup() {
   button = createButton("Play");
   button.mousePressed(togglePlayer);
   background(0);
-  blobby = new Blobby(10, 10);
+  blobby = new Blobby(10, 10, img); // pass the image to the blobby object
   terrain = new Terrain(0, w / scl, h / scl, scl, w, h);
   for (let x = 0; x < terrain.cols; x++) {
     terrain.terrain[x] = [];
@@ -43,20 +41,37 @@ function setup() {
   }
 }
 
+
+function preload() {
+  song = loadSound("./fluffy.mp3");
+  catimg = loadImage("nyancat.jpg")
+  img = loadImage('asteroid2.jpg'); // assign the image to the global variable img
+  blobby = new Blobby(200, 200, img); // create a new blobby object and pass the image to it
+}
+
 function draw() {
   camera(width / 2, height / 2, 500, width / 2, height / 2, 0, 0, 1, 0);
   translate(width / 2, height / 2);
   terrain.draw();
   terrain.updateTerrain();
   blobby.draw();
-
+  texture(catimg); // use the global variable img here instead of this.img
   let spectrum = fft.analyze(freqBins);
   console.log(spectrum)
   beginShape()
   let size = map(spectrum[0], 0, 255, 50, 200); // map size to lowest frequency bin
   if(size > 354){
-  // let x = map(spectrum[10], 0, 255, 0, width); 
-  // let y = map(spectrum[20], 0, 255, 0, height); 
-  blobby.update(500, 430, 100);
+    blobby.update(500, 430, 100);
+ 
   }
+  translate((-width/4 +1), -height/4,)
+  push();
+  texture(catimg);
+  rotateY(angle);
+  sphere(30, 30, 10);
+  pop();
+  
+  angle += 0.1;
+  endShape()
 }
+ 
